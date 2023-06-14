7476798dd938e4fc9cfc22ab66e2a59d0e227623
Aswanth Alakkadan <aswanth682@gmail.com>
2023-06-14 06:50:20 +0530

    form validated

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ snd = () => {
     window.alert("Send successfully ");
     location.reload();
 }
+validate = () => {
+    let x = document.forms["contact-form"]["nam"].value;
+    if(x==""){
+        window.alert("Please enter your name ");
+        document.forms["contact-form"]["nam"].focus();
+        return false;
+    }
+    let y = document.forms["contact-form"]["mail"].value;
+    if(y==""){
+        window.alert("Enter your email ");
+        document.forms["contact-form"]["mail"].focus();
+        return false;
+    }
+    let z = document.forms["contact-form"]["msg"].value;
+    if(z==""){
+        window.alert("Message cannot be empty");
+        document.forms["contact-form"]["msg"].focus();
+        return false;
+    }
+}
